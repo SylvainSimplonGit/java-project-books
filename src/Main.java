72cd14796d016956a8ed3b6b4046c7375b6caf06
SylvainSimplonDev <sylvain.grippon.devweb@gamil.com>
2019-10-11 15:53:47 +0200

    Compter le nombre de mots dans mots.txt

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,32 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Début de récupération");
+        HashMap listWords = readFileToArrayList("ressources/books/mots.txt");
+        System.out.println("Il y a " + listWords.size() + " mots différents dans " + "ressources/books/mots.txt");
+        System.out.println("Fin de récupération");
+    }
+
+    private static HashMap readFileToArrayList(String nameFile) {
+        HashMap<String, Integer> listWords = new HashMap<>();
+        int numberWord = 0;
+        try (Scanner scFile = new Scanner(new File(nameFile))) {
+            for (int i = 0; (scFile.hasNextLine()); ++i) {
+                // Pour chaque mot, incrémenter sa valeur
+                String word = scFile.nextLine();
+                if (word.length() > 0) {
+                    numberWord = (listWords.get(word) != null) ? listWords.get(word) : 0;
+                    listWords.put(word, numberWord + 1);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Une erreur est survenue !\n" + e.getLocalizedMessage());
+        }
+        return listWords;
     }
 }
