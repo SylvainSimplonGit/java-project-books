f5dc5e0af0d45b9ea3f5ebdeb4570e70959c7cee
SylvainSimplonDev <sylvain.grippon.devweb@gamil.com>
2019-10-15 21:39:33 +0200

    Ajout de commentaires

--- src/ListFile.java
+++ src/ListFile.java
@@ -4,6 +4,7 @@ public class ListFile {
     // --------------------------------------------------------------
     private String[] listOfFiles;
     private int numberOfFiles;
+    private int indexOfChosenFile;
 
     // --------------------------------------------------------------
     //      Constructor
@@ -14,18 +15,36 @@ public class ListFile {
 
     }
 
+    // --------------------------------------------------------------
+    //      Getter
+    // --------------------------------------------------------------
+    // Return the number of files from the list
     public int getNumberOfFiles() {
         return this.numberOfFiles;
     }
 
-    private void setNumberOfFiles() {
-        this.numberOfFiles = this.listOfFiles.length;
-    }
-
+    // Return the files list
     public String[] getListOfFiles() {
         return this.listOfFiles;
     }
 
+    // Return the index of chosen file
+    public int getIndexOfChosenFile() {
+        return this.indexOfChosenFile;
+    }
+
+    // --------------------------------------------------------------
+    //      Getter
+    // --------------------------------------------------------------
+    // Set the index of chosen file
+    public void setIndexOfChosenFile(int index) {
+        this.indexOfChosenFile = index;
+    }
+
+    // --------------------------------------------------------------
+    //      Public Methods
+    // --------------------------------------------------------------
+    // Add a file filename to the files list
     public void addFileToListOfFiles(String filename) {
         String[] newListOfFiles = new String[this.listOfFiles.length + 1];
         for (int i = 0; i != this.listOfFiles.length; ++i) {
@@ -36,6 +55,7 @@ public class ListFile {
         this.setNumberOfFiles();
     }
 
+    // Delete the file at the index from files list
     public void deleteFileFromListOfFiles(int index) {
         int indexNew = 0;
 
@@ -51,6 +71,15 @@ public class ListFile {
         this.setNumberOfFiles();
     }
 
+    // --------------------------------------------------------------
+    //      Private Methods
+    // --------------------------------------------------------------
+    // Set the number of file in files list
+    private void setNumberOfFiles() {
+        this.numberOfFiles = this.listOfFiles.length;
+    }
+
+    // Return the number of files in the file list
     private int calculateNumberOfFiles() {
         return this.listOfFiles.length;
     }
