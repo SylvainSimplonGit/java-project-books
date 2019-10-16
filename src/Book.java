4ff1726249da52419769aa1ce6ccf1520dbe1eab
SylvainSimplonDev <sylvain.grippon.devweb@gamil.com>
2019-10-16 15:02:42 +0200

    Ajout de la liste ordonnée

--- src/Book.java
+++ src/Book.java
@@ -1,82 +1,81 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Book {
     // --------------------------------------------------------------
     //      Declare attributes
     // --------------------------------------------------------------
-    //    private int numberOfWord;
     private int numberOfDifferentWord;
     private int numberOfTotalWord;
-    private String filename;
+    private ArrayList<Word> words;
 
     // --------------------------------------------------------------
     //      Constructor
     // --------------------------------------------------------------
     public Book(String pathFileName) {
-        filename = pathFileName;
-        readFileToArrayList(this.filename);
-//        numberOfDifferentWord = calculateNumberOfDifferentWord();
-//        numberOfWord =
+        readFileToArrayList(pathFileName);
     }
 
     // --------------------------------------------------------------
     //      Getters
     // --------------------------------------------------------------
     // Get the number of different words in a book
-    public int getNumberOfTotalWord() {
+    int getNumberOfTotalWord() {
         return this.numberOfTotalWord;
     }
 
     // Get the number of different words in a book
-    public int getNumberOfDifferentWord() {
+    int getNumberOfDifferentWord() {
         return this.numberOfDifferentWord;
     }
 
     // Get a list of words in the book sorted by number of occurrences
+    ArrayList getArrayListOfWordsSorted() {
+        ArrayList<Word> wordsSorted = new ArrayList<>();
+        for (int i = 0; i != this.words.size(); ++i) {
+            wordsSorted.add(i, this.words.get(i));
+        }
+        Collections.sort(wordsSorted);
+        return wordsSorted;
+    }
 
     // --------------------------------------------------------------
     //      Internal Methods
     // --------------------------------------------------------------
 
-    // Find the number of different word in file
-//    private int calculateNumberOfDifferentWord() {
-//        // Read file this.filename
-//        HashMap listWords = readFileToArrayList(this.filename);
-//        // Count the number of different words
-//        return listWords.size();
-//    }
-
     // Read file and return a HashMap contains different word in file
     private void readFileToArrayList(String nameFile) {
-        HashMap<String, Integer> listWords = new HashMap<>();
-        int numberWord = 0;
         int numberTotalWord = 0;
+        this.words = new ArrayList<>();
+
         try (Scanner scFile = new Scanner(new File(nameFile))) {
-            for (int i = 0; (scFile.hasNextLine()); ++i) {
+//            for (int i = 0; (scFile.hasNextLine()); ++i) {
+                while (scFile.hasNextLine()) {
+
+//                }
                 // Read each line of file
                 String word = scFile.nextLine();
                 // If the line isn't empty
                 if (word.length() > 0) {
+                    Word wordObject = new Word(word);
                     // If the word is already in the list, get number of times the word is seen, if not, get zero
-                    numberWord = (listWords.get(word) != null) ? listWords.get(word) : 0;
-                    // For each word, increment its value
-                    listWords.put(word, numberWord + 1);
+                    if (words.size() > 0 && words.contains(wordObject)) {
+                        int index = words.indexOf(wordObject);
+                        words.get(index).setNumberSeen(words.get(index).getNumberSeen() + 1);
+                    } else {
+                        words.add(wordObject);
+                    }
                     ++numberTotalWord;
                 }
             }
         } catch (FileNotFoundException e) {
             System.out.println("Une erreur est survenue !\n" + e.getLocalizedMessage());
         }
+        this.numberOfDifferentWord = words.size();
         this.numberOfTotalWord = numberTotalWord;
-        this.numberOfDifferentWord = listWords.size();
-
-//        return listWords;
     }
-
-    // Find a list of words sorted by number of times the word is seen
-
 
 }
