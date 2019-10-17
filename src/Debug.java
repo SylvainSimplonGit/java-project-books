d48d0034c16727c21a59d584d886ee1be4268501
SylvainSimplonDev <sylvain.grippon.devweb@gamil.com>
2019-10-17 10:02:25 +0200

    Ajout d'une méthode tracer des msg dans un fichier

--- src/Debug.java
+++ src/Debug.java
@@ -1,5 +1,16 @@
+import java.io.*;
+
 public class Debug {
     private static void showTrace(String function, String message) {
         System.out.println("[" + function + "] => " + message);
+    }
+
+    static void writeMessageInFile(String filename, String message) {
+        try (PrintStream fileOut = new PrintStream(new FileOutputStream(filename, true))) {
+            fileOut.println(message);
+
+        } catch(FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
